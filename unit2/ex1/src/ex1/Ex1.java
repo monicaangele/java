554dec13f50e8187ac94deacfd9efde5f65f62e8
monica 3 <afg.monii@gmail.com>
2025-01-22 08:04:12 -0600

    Update Ex1.java

--- unit2/ex1/src/ex1/Ex1.java
+++ unit2/ex1/src/ex1/Ex1.java
@@ -1,9 +1,25 @@
 package ex1;
 
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+        int number = 6;
+
+        System.out.print("Guess a number between 1 and 10: ");
+
+        int guess = Integer.parseInt(input.nextLine());
+
+        if (guess > number) {
+            System.out.println("Too high");
+        } else if (guess == number) {
+            System.out.println("You got it!");
+        } else {
+            System.out.println("Too low");
+        }
 
 	}
 
